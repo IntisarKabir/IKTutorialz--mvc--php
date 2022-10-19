da83374371797affdfd49265038ee94c4a4ef096
IntisarKabir <75949278+IntisarKabir@users.noreply.github.com>
2022-10-19 16:04:03 +0600

    Create Person.php

---
+++ routing_demo1/model/Person.php
@@ -0,0 +1,31 @@
+
+<?php
+/**
+ * Person Class
+ * Provides access to the "Person" database table.
+ *
+ * 
+ */
+class Person extends AbstractModel
+{
+    /**
+     *  Three columns,
+     *
+     *      - The ID
+     *      - The person name
+     *      - The person's favourite colour.
+     */
+    public static $data = array(
+        array(
+            'id' => 1,
+            'name' => 'Johnny Greensmith',
+            'fav_colour' => 'Green'
+        ),
+
+        array(
+            'id' => 2,
+            'name' => 'Mary Blueton',
+            'fav_colour' => 'Blue'
+        )
+    );
+}
